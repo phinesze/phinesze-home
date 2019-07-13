1a2cc3aa3af5c80a6f38d466a3f80ae086d92cb7
phinesze <phinesze@gmail.com>
2019-07-13 18:41:57 +0900

    link.jsを更新

--- src/data/links.js
+++ src/data/links.js
@@ -1,18 +1,6 @@
 const links = [
-    {"id": "0", "img": require("../images/link/github.png"), "text": "LINK"},
-    {"id": "1", "img": require("../images/link/github.png"), "text": "LINK"},
-    {"id": "2", "img": require("../images/link/github.png"), "text": "LINK"},
-    {"id": "3", "img": require("../images/link/github.png"), "text": "LINK"},
-    {"id": "4", "img": require("../images/link/github.png"), "text": "LINK"},
-    {"id": "5", "img": require("../images/link/github.png"), "text": "LINK"},
-    {"id": "6", "img": require("../images/link/github.png"), "text": "LINK"},
-    {"id": "7", "img": require("../images/link/github.png"), "text": "LINK"},
-    {"id": "8", "img": require("../images/link/github.png"), "text": "LINK"},
-    {"id": "9", "img": require("../images/link/github.png"), "text": "LINK"},
-    {"id": "10", "img": require("../images/link/github.png"), "text": "LINK"},
-    {"id": "11", "img": require("../images/link/github.png"), "text": "LINK"},
-    {"id": "12", "img": require("../images/link/github.png"), "text": "LINK"},
-    {"id": "13", "img": require("../images/link/github.png"), "text": "LINK"}
+    {"id": "0", "img": require("../images/link/github.png"), "text": "GitHub"},
+    {"id": "1", "img": null, "text": "クラウドワークス"},
 ];
 
 export default links;
