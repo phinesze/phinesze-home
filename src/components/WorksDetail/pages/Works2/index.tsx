28db79d992d77fb267d95347f1ba9db9e0b271ca
phinesze <phinesze@gmail.com>
2019-08-15 18:07:46 +0900

    WorksDetail画面に遷移時にフェードインを追加

---
+++ src/components/WorksDetail/pages/Works2/index.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+/**
+ *
+ */
+export default class Works2 extends React.Component {
+
+    constructor(props: {}) {
+        super(props);
+    }
+
+    /**
+     * レンダリングを行う。
+     */
+    render(): JSX.Element {
+        return <div>PAGE2</div>;
+    }
+}
--- src/data/works.ts
+++ src/data/works.ts
@@ -1,9 +1,10 @@
 import {SquareMenuItemParams} from "../components/common/SquareMenu/SquareMenuItem";
 import Works1 from "../components/WorksDetail/pages/Works1/index";
+import Works2 from "../components/WorksDetail/pages/Works2/index";
 
 const works: SquareMenuItemParams[] = [
     {id: "0", img: "", url: '/works/coltvox', text: "ウェブサイトの作成", innerComponent: new Works1({})},
-    {id: "1", img: "", url: "/works/sumashin", text: "あああああああああああああああああああああああああああああああああああああああああああああ"},
+    {id: "1", img: "", url: "/works/sumashin", text: "あああああああああああああああああああああああああああああああああああああああああああああ", innerComponent: new Works2({})},
 ];
 
 export default works;
