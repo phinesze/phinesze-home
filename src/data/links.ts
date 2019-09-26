57693f195d9139e1b64a23094f47c5b5e7d0252d
phinesze <phinesze@gmail.com>
2019-09-27 00:21:21 +0900

    linksにはてなブログを追加

--- src/data/links.ts
+++ src/data/links.ts
@@ -2,7 +2,8 @@ import {SquareMenuItemParams} from "../components/common/SquareMenu/SquareMenuIt
 
 const links: SquareMenuItemParams[] = [
     {"id": "0", "img": require("../images/link/github.png"), url: "https://github.com/phinesze", "text": "GitHub"},
-    {"id": "1", "img": require("../images/link/crowdworks.svg"), url: "https://crowdworks.jp/public/employees/1883807", "text": "クラウドワークス"},
+    {"id": "1", "img": require("../images/link/logo-icon.svg"), url: "https://phinesze.hatenablog.com/", "text": "はてなブログ"},
+    {"id": "2", "img": require("../images/link/crowdworks.svg"), url: "https://crowdworks.jp/public/employees/1883807", "text": "クラウドワークス"},
 ];
 
 export default links;
